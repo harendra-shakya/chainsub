90506ac3f895934d15ea2da74bf92340e491c34e
Mugrebot <mikeongay@icloud.com>
2023-02-06 09:58:16 -0500

    fixed ipfsprofile.sol

--- backend/contracts/IpfsProfile.sol
+++ backend/contracts/IpfsProfile.sol
@@ -1,69 +1,206 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
-
-//add reentrancy guard
-
-import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-
-contract IpfsProfile is OwnableUpgradeable, ReentrancyGuardUpgradeable {
-
-    event Received(address, uint);
-    event CoffeeBought(address);
-    event Received(uint);
-
-    mapping (address => string) public addressToCid;
-
-    //constructor
-     function initialize() public initializer {
-        __Ownable_init();
-    }
-    
-
-    //add a CID to the contract
-
-    function addCID(string memory cidraw) public onlyOwner {
-        //set the cidToAddress mapping to the address of the owner
-        addressToCid[msg.sender] = cidraw;
-    }
-
-    function updateCID(string memory new_cidraw) public onlyOwner {
-        addressToCid[msg.sender] = new_cidraw;
-    }
-
-
-    //get the CID
-    function getCID() public view returns (string memory) {
-        string memory profileCid= addressToCid[msg.sender] ;
-        return profileCid;
-    }
-
-        function fundme() public payable {
-        emit Received(msg.value);
-    }
-
-    receive() external payable  { 
-        fundme();
-    }
-
-    fallback() external payable {
-        fundme();
-    }
-
-    //add a withdraw function
-    function withdraw() public onlyOwner nonReentrant() {
-        uint balance = address(this).balance;
-        payable(msg.sender).transfer(balance);
-    }
-
-    function buyCoffee() public payable {
-        require(msg.value >= 0.01 ether, "Not enough!");
-        //send 0.01 ETH to the owner
-        payable(owner()).transfer((0.01 ether));
-        emit CoffeeBought(msg.sender);
-    }
-
-
-
-
-}
+[
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "CoffeeBought",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint8",
+				"name": "version",
+				"type": "uint8"
+			}
+		],
+		"name": "Initialized",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "Received",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "Received",
+		"type": "event"
+	},
+	{
+		"stateMutability": "payable",
+		"type": "fallback"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "cidraw",
+				"type": "string"
+			}
+		],
+		"name": "addCID",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "addressToCid",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "buyCoffee",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "fundme",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getCID",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "initialize",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "new_cidraw",
+				"type": "string"
+			}
+		],
+		"name": "updateCID",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "withdraw",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"stateMutability": "payable",
+		"type": "receive"
+	}
+]
